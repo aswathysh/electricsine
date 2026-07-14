7d9952ae570811c0ac37833875ff51a1b3cb5a12
“sreedershsudarsh” <sreedershsudarsh9900@gmail.com>
2026-07-14 22:19:42 +0530

    sitemap crt and order history design crt

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,29 +1,59 @@
 export default function sitemap() {
-  const baseUrl = 'https://electricsine.com';
+  const baseUrl = "https://www.electricsine.com";
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
+      changeFrequency: "yearly",
+      priority: 1.0,
     },
     {
-      url: `${baseUrl}/courses`, 
+      url: `${baseUrl}/courses`,
       lastModified: new Date(),
-      changeFrequency: 'weekly', 
-      priority: 0.9, 
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
     {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}/aboutus`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/exam`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/practice`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contactus`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/privacypolicy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/terms-and-conditions`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
       priority: 0.5,
     },
   ];
